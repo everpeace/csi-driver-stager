1b52ebdb95308aa5b12fe694d93f08bb6e2dbbdc
Shingo Omura <everpeace@gmail.com>
2020-01-20 15:13:57 +0900

    stop klog complaining "ERROR: logging before flag.Parse"

--- go.mod
+++ go.mod
@@ -21,5 +21,6 @@ require (
 	k8s.io/api v0.17.0
 	k8s.io/apimachinery v0.17.1-beta.0
 	k8s.io/client-go v0.17.0
+	k8s.io/klog v1.0.0
 	k8s.io/utils v0.0.0-20191114184206-e782cd3c129f
 )
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/everpeace/csi-driver-stager/cmd"
+import (
+	"flag"
+	"github.com/everpeace/csi-driver-stager/cmd"
+	"k8s.io/klog"
+)
 
 var (
 	Version  string
@@ -10,6 +14,13 @@ var (
 func init() {
 	cmd.Version = Version
 	cmd.Revision = Revision
+
+	// hack to make flag.Parsed return true such that glog is happy
+	// about the flags having been parsed
+	fs := flag.NewFlagSet("klog", flag.ContinueOnError)
+	klog.InitFlags(fs)
+	_ = fs.Parse([]string{"-logtostderr=true", "-v=0"})
+	flag.CommandLine = fs
 }
 
 func main() {
